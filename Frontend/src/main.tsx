2e3b33dc52811fe90ffb1c19ea8d2f3989219332
gordon-coder <gordonpeng27@gmail.com>
2025-08-14 16:03:26 +0800

    added routing

--- Frontend/src/main.tsx
+++ Frontend/src/main.tsx
@@ -1,10 +1,20 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router";
+import './index.css';
+import App from './App';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const router = createBrowserRouter([
+  {
+    path: "/",
+    Component: App,
+  },
+]);
+
+const root = document.getElementById("root")!;
+
+createRoot(root).render(
+  <RouterProvider router={router} />,
+);

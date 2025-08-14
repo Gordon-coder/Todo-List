c12dfb4c91e252dd0e177a66e6dffd1ecf99eca7
gordon-coder <gordonpeng27@gmail.com>
2025-08-14 16:11:06 +0800

    added Navbar

---
+++ Frontend/src/NavBar.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+import NavDropdown from "react-bootstrap/NavDropdown";
+
+function NavBar() {
+  return (
+    <Navbar bg="light" expand="lg" className="p-2">
+      <Navbar.Brand href="#home">Todo List</Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="me-auto">
+          <Nav.Link href="/">Home</Nav.Link>
+          <Nav.Link href="/about">About</Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+  );
+}
+
+export default NavBar;
--- Frontend/src/main.tsx
+++ Frontend/src/main.tsx
@@ -3,6 +3,7 @@ import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router";
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import App from './App';
 
